7fc0f7723d573d2f789182d02ee3fa6f3c1c90f7
Chris Weaver <chris.weaver@readytalk.com>
2015-12-11 10:33:17 -0700

    improve stats code

--- home/bin/rows.pl
+++ home/bin/rows.pl
@@ -1,19 +1,23 @@
 #!/usr/bin/perl
 
-my %cols = {};
+my %colByStat = {};
+my %statByCol = {};
 
 while (<>) {
   chomp;
-  my ($day, $time, $level, $thread, $class, $stat, $ssrc, $value) = split / /;
-  my $key = "$stat" . "_" . "$ssrc";
-  if (not exists $cols{$key}) {
-    $cols{$key} = keys(%cols);
-  }
-  print "\n$day $time,$key:$cols{$key}";
-  for (my $c=1; $c < keys(%cols); $c++) {
-    print ",";
-    if ($cols{$key} eq $c) {
-      print $value;
+  my ($day, $time, $level, $thread, $class, $marker, $statsString) = split / /;
+  my @stats = split /,/, $statsString;
+  my %statHash = {};
+  foreach my $stat (@stats) {
+    my ($statKey, $statValue) = split /:/, $stat;
+    if (not exists $colByStat{$statKey}) {
+      $colByStat{$statKey} = keys(%colByStat);
+      $statByCol{$colByStat{$statKey}} = $statKey
     }
+    $statHash{$colByStat{$statKey}} = $statValue;
+  }
+  print "\n$day $time";
+  for (my $c=1; $c < keys(%colByStat); $c++) {
+    print ",$statHash{$c}";
   }
 }

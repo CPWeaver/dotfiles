ce914438a53a43f3d6b44b0694c306038524e46e
Chris Weaver <chrisweaver@invisionapp.com>
2023-07-07 12:06:10 -0600

    Updates to linux setup

--- home/bin/extra_setup.sh
+++ home/bin/extra_setup.sh
@@ -10,7 +10,10 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
-  . ~/.bash_profile
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
   nvm i --lts
   npm i -g yarn bash-language-server
 

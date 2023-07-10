b3c89aa90b67c0c01e7b08e02c08849ad71608d3
Chris Weaver <chrisweaver@invisionapp.com>
2023-07-10 08:43:18 -0600

    more linux setup

--- home/bin/extra_setup.sh
+++ home/bin/extra_setup.sh
@@ -6,7 +6,10 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   echo "Performing extra setup for linux..."
   # Linux setup
   sudo apt-get update
-  sudo apt-get install entr vim sed coreutils fzf libutf8proc-dev libevent-dev bison autotools-dev automake libncurses5-dev cmake python3 pip bat ripgrep
+  sudo apt-get install entr vim sed coreutils \
+    fzf libutf8proc-dev libevent-dev bison \
+    autotools-dev automake libncurses5-dev cmake \
+    python3 pip bat ripgrep silversearcher-ag
 
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
@@ -17,15 +20,15 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   nvm i --lts
   npm i -g yarn bash-language-server
 
-  mkdir -p ~/dev/tools
+  pip install pynvim
+  go install github.com/nametake/golangci-lint-langserver@latest
+  go install github.com/go-delve/delve/cmd/dlv@latest
 
+  mkdir -p ~/dev/tools
   install_tools.sh
 
   bash <(curl -s https://raw.githubusercontent.com/CPWeaver/dot_vim/master/install)
 
-  pip install pynvim
-
-  go install github.com/nametake/golangci-lint-langserver@latest
 fi
 
 echo; echo "Extra setup complete!"

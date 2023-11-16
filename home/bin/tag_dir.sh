4ea577045cc67e338821eb2e35ef6d056c65bddc
Chris Weaver <chrisweaver@invisionapp.com>
2023-11-16 10:39:17 -0700

    Slightly Improved tagging

--- home/bin/tag_dir.sh
+++ home/bin/tag_dir.sh
@@ -3,17 +3,23 @@
 #first, apply sidecar data to original images
 IFS=$'\n'
 
+d=$(basename "$1")
+if  [[ $d != [[:digit:]]* ]] ; then
+  echo "Argument should be a year directory, e.g. 2006"
+  exit 1
+fi
+
+
 for dir in $(find "$1" -type d -depth 2)
 do
   subject=$(basename "$dir")
   echo "dir: $dir; subject: $subject"
 
-  if  [[ $subject == 201* ]] ; then
+  if  [[ $subject =~ ^[0-9] ]] ; then
     echo "Error: non-subject '$subject'"
     exit 1
   fi
 done
-
 
 for dir in $(find "$1" -type d -depth 2)
 do
@@ -24,7 +30,7 @@ do
   # subject="${dir#$1/}"
   echo "dir: $dir; subject: $subject"
 
-  if  [[ $subject == 201* ]] ; then
+  if  [[ $subject =~ ^[0-9] ]] ; then
     echo "Found non-subject: $subject"
     exit 1
   fi

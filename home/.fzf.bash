6d96274884e12087b991e1e0a50d38448a4a5ac1
Chris Weaver <chris.weaver@assured.claims>
2024-03-29 09:43:45 -0600

    minor updates

--- home/.fzf.bash
+++ home/.fzf.bash
@@ -4,10 +4,4 @@ if [[ ! "$PATH" == */Users/chris/dev/tools/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/Users/chris/dev/tools/fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-source "/Users/chris/dev/tools/fzf/shell/completion.bash"
-
-# Key bindings
-# ------------
-source "/Users/chris/dev/tools/fzf/shell/key-bindings.bash"
+eval "$(fzf --bash)"
